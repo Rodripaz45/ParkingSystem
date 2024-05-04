d98e41ab5dce4dad77785afdac6d2518a16c1e36
Rodrigo Paz <rodripaz45@gmail.com>
2024-05-04 18:08:17 -0400

    status reservas

--- src/controllers/reservaciones.controller.js
+++ src/controllers/reservaciones.controller.js
@@ -2,16 +2,58 @@
 import { pool } from "../db.js";
 
 export const createReservacion = async (req, res) => {
-    const { fk_id_garaje, fk_id_auto, start_time, end_time, horas_disponibles, status, precio } = req.body;
+    const { fk_id_garaje, fk_id_auto, start_time, end_time, horas_disponibles, precio } = req.body;
     try {
         const result = await pool.query(
-            'INSERT INTO reservaciones (fk_id_garaje, fk_id_auto, horainicio, horafin, horasdisponibles, estado, precio) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id_reservacion',
-            [fk_id_garaje, fk_id_auto, start_time, end_time, horas_disponibles, status, precio]
+            'INSERT INTO reservaciones (fk_id_garaje, fk_id_auto, horainicio, horafin, horasdisponibles, estado, precio) VALUES ($1, $2, $3, $4, $5, "PENDIENTE", $6) RETURNING id_reservacion',
+            [fk_id_garaje, fk_id_auto, start_time, end_time, horas_disponibles, precio]
         );
         const reservacionId = result.rows[0].id;
         res.status(201).json({ id: reservacionId, message: 'Reservación creada correctamente' });
     } catch (error) {
         console.error('Error al crear reservación:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const confirmReservacion = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(
+            'UPDATE reservaciones SET estado = $1 WHERE id_reservacion = $2',
+            ['CONFIRMADO', id]
+        );
+        res.json({ message: 'Reservación confirmada correctamente' });
+    } catch (error) {
+        console.error('Error al confirmar reservación:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const rejectReservacion = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(
+            'UPDATE reservaciones SET estado = $1 WHERE id_reservacion = $2',
+            ['RECHAZADO', id]
+        );
+        res.json({ message: 'Reservación rechazada correctamente' });
+    } catch (error) {
+        console.error('Error al rechazar reservación:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const cancelReservacion = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(
+            'UPDATE reservaciones SET estado = $1 WHERE id_reservacion = $2',
+            ['CANCELADA', id]
+        );
+        res.json({ message: 'Reservación rechazada correctamente' });
+    } catch (error) {
+        console.error('Error al rechazar reservación:', error);
         res.status(500).send('Error en el servidor');
     }
 };
@@ -22,6 +64,58 @@ export const getAllReservaciones = async (req, res) => {
         res.json(result.rows);
     } catch (error) {
         console.error('Error al obtener reservaciones:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const getReservacionesConfirmadas = async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM reservaciones WHERE estado = $1',
+            ['CONFIRMADO']
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener reservaciones confirmadas:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const getReservacionesRechazadas = async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM reservaciones WHERE estado = $1',
+            ['RECHAZADO']
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener reservaciones rechazadas:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const getReservacionesPendientes = async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM reservaciones WHERE estado = $1',
+            ['PENDIENTE']
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener reservaciones pendientes:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
+export const getReservacionesCanceladas = async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM reservaciones WHERE estado = $1',
+            ['CANCELADA']
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener reservaciones rechazadas:', error);
         res.status(500).send('Error en el servidor');
     }
 };
@@ -59,6 +153,22 @@ export const updateReservacion = async (req, res) => {
         res.status(500).send('Error en el servidor');
     }
 };
+
+export const updatePrecioReservacion = async (req, res) => {
+    const { id } = req.params;
+    const { precio } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE reservaciones SET precio = $1 WHERE id_reservacion = $2',
+            [precio, id]
+        );
+        res.json({ message: 'Precio de la reservación actualizado correctamente' });
+    } catch (error) {
+        console.error('Error al actualizar precio de la reservación:', error);
+        res.status(500).send('Error en el servidor');
+    }
+};
+
 
 export const deleteReservacion = async (req, res) => {
     const { id } = req.params;
--- src/routes/reservaciones.routes.js
+++ src/routes/reservaciones.routes.js
@@ -1,6 +1,6 @@
 //@ts-check
 import { Router } from "express";
-import { createReservacion, getAllReservaciones, getReservacionById, updateReservacion, deleteReservacion } from "../controllers/reservaciones.controller.js";
+import { createReservacion, getAllReservaciones, getReservacionById, updateReservacion, deleteReservacion, updatePrecioReservacion, confirmReservacion, rejectReservacion, cancelReservacion, getReservacionesConfirmadas, getReservacionesRechazadas, getReservacionesPendientes, getReservacionesCanceladas } from "../controllers/reservaciones.controller.js";
 
 const router = Router();
 
@@ -9,6 +9,14 @@ router.get('/getAllReservaciones', getAllReservaciones);
 
 router.get('/getReservacionById/:id', getReservacionById);
 router.put('/updateReservacion/:id', updateReservacion);
+router.put('/updatePrecioReservacion/:id', updatePrecioReservacion);
+router.put('/confirmReservacion/:id', confirmReservacion);
+router.put('/rejectReservacion/:id', rejectReservacion);
+router.put('/cancelReservacion/:id', cancelReservacion);
+router.get('/getReservacionesConfirmadas', getReservacionesConfirmadas);
+router.get('/getReservacionesRechazadas', getReservacionesRechazadas);
+router.get('/getReservacionesPendientes', getReservacionesPendientes);
+router.get('/getReservacionesCanceladas', getReservacionesCanceladas);
 router.delete('/deleteReservacion/:id', deleteReservacion);
 
 export default router;
